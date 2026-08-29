a4f4f35c3ec9aecebfdf300ef7bc7cce51b73696
sankargavate-cmd <sankargavate@gmail.com>
2026-08-29 16:28:36 +0530

    Delete backend-backup-before-replace directory

--- backend-backup-before-replace/src/middlewares/notFound.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export function notFound(req: Request, res: Response, _next: NextFunction) {
-  res.status(404).json({
-    success: false,
-    message: `Route not found: ${req.method} ${req.originalUrl}`,
-  });
-}
